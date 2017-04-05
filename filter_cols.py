cdc5e4682f3e6dca30077995b9116902cb950090
dave <dwheelerau@gmail.com>
2017-04-05 14:18:59 +1200

    mod filter_cols to exclude primers

--- filter_cols.py
+++ filter_cols.py
@@ -4,7 +4,15 @@ import sys
 import csv
 
 # Fitler param
-pos_limit = 230  # remove low quality seq
+left_size_limit = 250  # remove low quality seq
+# primer lengths to chop off this sequencegene:  gene: (forward, reverese)
+primer_dict = {'AAT1a': (21, 370),
+               'ACC1': (20, 382),
+               'ADP1': (22, 375),
+               'MPI': (21, 377),
+               'SYA1': (20, 363),
+               'VPS13': (20, 395),
+               'ZWF1b': (22, 385)}
 
 left = sys.argv[1]
 sample = left.split('/')[-2]
@@ -14,11 +22,6 @@ outfile_csv = csv.writer(outfile)
 final_outfile = open('final_results/final_table.csv', 'a')
 final_csv = csv.writer(final_outfile)
 
-# add header
-#with open('final_results/final_table.csv') as f:
-#    csv_reader = csv.reader(f)
-#    final_csv.writerow(csv_reader.next())
-
 with open(left) as f:
     for line in f:
         bits = line.strip().split("\t")
@@ -27,8 +30,10 @@ with open(left) as f:
             header.insert(0, "Sample")
             outfile_csv.writerow(header)
         else:
+            gene = bits[0]
+            primer_f = primer_dict[gene][0]
             pos = int(bits[1])
-            if pos <= pos_limit:
+            if pos <= left_size_limit and pos > primer_f:
                 # len(14) is if the a/t etc non-ref col is added
                 # turn this off and just output all data
                 if len(bits) == 14:
@@ -42,6 +47,7 @@ with open(left) as f:
                 output.insert(0, sample)
                 outfile_csv.writerow(output)
                 final_csv.writerow(output)
+
 right = sys.argv[2]
 with open(right) as f:
     for line in f:
@@ -49,8 +55,11 @@ with open(right) as f:
         if line[0] == "l":
             pass
         else:
+            gene = bits[0]
+            primer_r = primer_dict[gene][1]
+
             pos = int(bits[1])
-            if pos > pos_limit:
+            if pos > left_size_limit and pos < primer_r:
                 # as above
                 if len(bits) == 14:
                     output = bits[:5] + bits[9:]
