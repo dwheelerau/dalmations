0c20ad6d7189791a85e4a3cf3884d0c7289d13c4
Dave Wheeler <dwheelerau@gmail.com>
2017-04-20 18:59:22 +1200

    still brocken

--- create_sequences.py
+++ create_sequences.py
@@ -12,14 +12,25 @@ for fil in sample_files:
     best = 0
     flag = 0
     result_dict = {}
-    with open(fil) as f:
+    with open("./final_genotypes/FJ9-S_S16_&_P1-80-20_S36.csv") as f:
         csv_reader = csv.reader(f)
         for row in csv_reader:
-            if row[0] == 'percentage':
-                flag == 1
+            if len(row) == 0:
+                continue
+            if row[0] == 'percent':
+                print "flag %s best %s" % (best, flag)
+                flag = 1
+                continue
             if flag == 1 and best == 0:
+                print "flag %s best %s" % (best, flag)
                 best = int(row[0])
                 gene = row[1]
-                result_dict[gene] = bits
+                result_dict[gene] = [row]
+                continue
+            if int(row[0]) == best:
+                if gene in result_dict:
+                    result_dict[gene].append(row)
+                else:
+                    result_dict[gene] = [row]
             else:
                 pass
