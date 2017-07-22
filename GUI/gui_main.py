a3d459d0f401ef4684463fab99d1bfe2d44a3883
Dave Wheeler <dwheelerau@gmail.com>
2017-07-23 11:37:18 +1200

    added some actions to buttons

--- GUI/gui_main.py
+++ GUI/gui_main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
 
-
 import sys
+import os
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
+from subprocess import Popen
 
 
 class Outer(QtGui.QMainWindow):
@@ -12,7 +13,6 @@ class Outer(QtGui.QMainWindow):
         super(Outer, self).__init__()
 
         self.initUI()
-
 
     def initUI(self):
 
@@ -42,6 +42,7 @@ class Outer(QtGui.QMainWindow):
         self.move((screen.width()-size.width())/2,
                   (screen.height()-size.height())/2)
 
+
 class Block(QtGui.QWidget):
 
     def __init__(self, parent):
@@ -49,11 +50,8 @@ class Block(QtGui.QWidget):
 
         self.initBlock()
 
-
     def initBlock(self):
 
-        demultLab = QtGui.QLabel('De-multiplex')
-        demultLab.setAlignment(Qt.AlignCenter)
         loadLab = QtGui.QLabel('Load')
         loadLab.setAlignment(Qt.AlignCenter)
         dataLab = QtGui.QLabel('Data')
@@ -69,29 +67,50 @@ class Block(QtGui.QWidget):
         #grid.setSpacing(10)
         #grid.setColumnStretch(0,1)
 
-        grid.addWidget(demultLab, 0, 0)
+        #grid.addWidget(demultLab, 0, 0)
         # grid.addWidget(titleEdit, 1, 1)
-        demultButton = QtGui.QPushButton('De-multiplex')
-        grid.addWidget(demultButton, 1, 0)
+        #demultButton = QtGui.QPushButton('De-multiplex')
+        #grid.addWidget(demultButton, 1, 0)
+        #demultButton.clicked.connect(self.demultButtonClk)
 
-        grid.addWidget(loadLab, 0, 1)
+        grid.addWidget(loadLab, 0, 0)
         #grid.addWidget(authorEdit, 2, 1)
         loadButton = QtGui.QPushButton('Load')
-        grid.addWidget(loadButton, 1, 1)
+        grid.addWidget(loadButton, 1, 0)
+        loadButton.clicked.connect(self.loadButtonClk)
 
-        grid.addWidget(dataLab, 0, 2)
+        grid.addWidget(dataLab, 0, 1)
         dataButton = QtGui.QPushButton('Data')
-        grid.addWidget(dataButton, 1, 2)
+        grid.addWidget(dataButton, 1, 1)
+        dataButton.clicked.connect(self.dataButtonClk)
 
-        grid.addWidget(logLab, 0, 3)
+        grid.addWidget(logLab, 0, 2)
         logButton = QtGui.QPushButton('Log')
-        grid.addWidget(logButton, 1, 3)
+        grid.addWidget(logButton, 1, 2)
+        logButton.clicked.connect(self.logButtonClk)
 
         # this stredtches over 5 rows
         #grid.addWidget(review, 3, 0)
         #grid.addWidget(reviewEdit, 3, 1, 5, 1)
 
         self.setLayout(grid)
+
+    def loadButtonClk(self):
+        # subprocess.Popen("./test.py", arg)
+        ddir = QtGui.QFileDialog.getExistingDirectory(self, "Get Dir PAth")
+        # ddir is a QString containing the path to the directory you
+        # selected
+        args = ['./test.py', ddir]
+        Popen(args)
+
+    def dataButtonClk(self):
+        # subprocess.Popen("./test.py", arg)
+        Popen("./test.py")
+
+    def logButtonClk(self):
+        # subprocess.Popen("./test.py", arg)
+        Popen("./test.py")
+
 
 def main():
 
