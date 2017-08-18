391bafc13c8fcffa581a227cf29146ccc9cc0945
Dave Wheeler <dwheelerau@gmail.com>
2017-08-18 14:18:57 +1200

    modified way SC sequences are made

--- create_sequences.py
+++ create_sequences.py
@@ -89,6 +89,21 @@ def remove_primers(gene, seq):
     seq = seq[cords[0]: cords[1] - 1]
     return seq
 
+
+def check_genotype(calls):
+    '''checks calls,tests 50/50 or 100/0, returns 0 if yes and 1 if no'''
+    calls_int = [int(c) for c in calls]
+    calls_int.sort(reverse=True)
+    # this should be >85 if 100/0
+    if calls_int[0] >= 93:
+        return 0
+    # this should be >45 if 55/45 or 50/50 ie second value is smallest
+    elif calls_int[1] >= 43:
+        return 0
+    else:
+        print(calls_int)
+        return 1
+
 # make sure we are in the base directory in case run from GUI
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -119,13 +134,20 @@ concat_order.sort()
 # are not in the summary tables (these only contain non-ref mix
 # calls). Modify the var nts using the same fn. These contain no primers.
 all_seq_calls = {}
-# THis is a fix!
+# this is only used for the single col calls, so I'm going to lowercase
+# anything here that has a strange genotpye (ie ! 100/0 or 50/50), this will
+# obviosuly effect mix genotypes but that is OK because those are ignored
 with open('./final_results/final_table_python.csv') as f:
     csv_reader = csv.reader(f)
     csv_reader.next()  # dump the header
     for row in csv_reader:
         sample = row[0]
         call = evolve_ref(row[-2])  # if non-ref will be in this col
+        obs_calls = row[6:10]
+        if check_genotype(obs_calls) == 1:
+            # lowercase because calls are not 100/0 or 50/50
+            # only effects single col calls as this dict is only used for those
+            call = call.lower()
         if sample in all_seq_calls:
             all_seq_calls[sample].append(call)
         else:
