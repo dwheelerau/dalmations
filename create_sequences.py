2f043712b57f0170070e7f15d782fdc62fb59472
Dave Wheeler <dwheelerau@gmail.com>
2017-08-19 12:46:31 +1200

    major refrac, needs testing

--- create_sequences.py
+++ create_sequences.py
@@ -132,26 +132,55 @@ concat_order.sort()
 # make a dict of all mlst posns for single sample calls which
 # are not in the summary tables (these only contain non-ref mix
 # calls). Modify the var nts using the same fn. These contain no primers.
-all_seq_calls = {}
+single_seq_calls = {}
 # this is only used for the single col calls, so I'm going to lowercase
 # anything here that has a strange genotpye (ie ! 100/0 or 50/50), this will
 # obviosuly effect mix genotypes but that is OK because those are ignored
+# changed this to be dict of samples and genes
 with open('./final_results/final_table_python.csv') as f:
     csv_reader = csv.reader(f)
     csv_reader.next()  # dump the header
     for row in csv_reader:
         sample = row[0]
+        gene = row[1]
+        pos = row[2]
         call = evolve_ref(row[-2])  # if non-ref will be in this col
         obs_calls = row[6:10]
+        # I only want this for single col calls
         if check_genotype(obs_calls) == 1:
-            # lowercase because calls are not 100/0 or 50/50
-            # only effects single col calls as this dict is only used for those
             call = call.lower()
-        if sample in all_seq_calls:
-            all_seq_calls[sample].append(call)
-        else:
-            all_seq_calls[sample] = [call]  # a list of posns
+        # lowercase because calls are not 100/0 or 50/50
+        # only effects single col calls as this dict is only used for those
 
+        if sample in single_seq_calls:
+            if gene in single_seq_calls[sample]:
+                single_seq_calls[sample][gene][pos] = call
+            else:
+                single_seq_calls[sample][gene] = {pos: call}
+        else:
+            single_seq_calls[sample] = {gene: {pos: call}}
+
+# not effecient
+mix_seq_calls = {}
+with open('./final_results/final_table_python.csv') as f:
+    csv_reader = csv.reader(f)
+    csv_reader.next()  # dump the header
+    for row in csv_reader:
+        sample = row[0]
+        gene = row[1]
+        pos = row[2]
+        call = evolve_ref(row[-2])  # if non-ref will be in this col
+        obs_calls = row[6:10]
+        if sample in mix_seq_calls:
+            if gene in mix_seq_calls[sample]:
+                mix_seq_calls[sample][gene][pos] = call
+            else:
+                mix_seq_calls[sample][gene] = {pos: call}
+        else:
+            mix_seq_calls[sample] = {gene: {pos: call}}
+
+
+# make a copy of this to 
 # make the reference sequence just once
 # TODO: this probably is not what I want
 refseq = [remove_primers(gene, "".join(seq_dict[gene]))
@@ -166,38 +195,66 @@ single_seq_used = []
 for fil in sample_files:
     result_dict = {}
     target_csv = "%s/%s" % (genotype_dir, fil)
+    single_sample_name = fil.split('_&_')[0]
+    mix_sample_name = fil.split('_&_')[1].split('.csv')[0]
+    # do the single sequence first, then use the derived genotpyes to morp
+    # this into a new dervied seq for mix col
+    # the derived nt calls are in these files
     with open(target_csv) as f:
         data = [data for data in csv.reader(f)]
-    seq_dict_mix = copy.deepcopy(seq_dict)
+    # seq_dict_mix = copy.deepcopy(seq_dict)
     scores = []
+    # this modifies single seq seq to contain mix calls
     for row in data:
         gene = row[1]
-        pos = int(row[2]) - 1
-        seq_dict_mix[gene][pos] = evolve_ref(row[13])
+        # pos = (row[2]) - 1
+        pos = row[2]
+        # seq_dict_mix[gene][pos] = evolve_ref(row[13])
+        mix_seq_calls[mix_sample_name][gene][pos] = evolve_ref(row[13])
         scores.append(float(row[16]))
-    # FJ9-S_S16 WARNING - this requres this format?
     percent_single = int(row[0])
     percent_mix = 100 - percent_single
-    single_sample_name = fil.split('_&_')[0]
     # this doesnt work with unknow as their format is:12-0056
     # sample_name = single_sample_name.split('-')[0]
     # just use the first part
     sample_name = single_sample_name  # tmp fix
-    confidence_score = make_confidence_score(scores)
-    single_sample_id = "%s:SC:%s:%.1f" % (sample_name, percent_single,
-                                          confidence_score)
-
-    mix_sample_id = "%s:IND:%s:%.1f" % (sample_name, percent_mix,
-                                        confidence_score)
+    # confidence_score = make_confidence_score(scores)
+    # single_sample_id = "%s:SC:%s:%.1f" % (sample_name, percent_single)  #,
+    #                                      # confidence_score)
+    mix_sample_id = "%s:IND:%s" % (sample_name, percent_mix)
+    # single_sample_id = "%s:SC:%s:%.1f" % (sample_name, percent_single)  #,
+                                        # confidence_score)
+    single_sample_id = "%s:SC:%s" % (sample_name, percent_single)
     # make the concatinated MLSTs single col
     if sample_name not in single_seq_used:
-        singleseq = all_seq_calls[single_sample_name]
+        # singleseq = all_seq_calls[single_sample_name]
+        singleseq = []
+        for gene in concat_order:
+            # need to sort the positions, NOTE keys are strings!!!!
+            ordered_pos = [int(p)
+                           for p in single_seq_calls[single_sample_name][gene].keys()]
+            ordered_pos.sort()
+            for pos in ordered_pos:
+                for nt in single_seq_calls[single_sample_name][gene][str(pos)]:
+                    singleseq.append(nt)
         singleseq = write_seq(single_sample_id, "".join(singleseq))
         seq_outfile.write(singleseq)
         single_seq_used.append(sample_name)
     # make the concatinated MLSTs mix col
-    mixseq = [remove_primers(gene, "".join(seq_dict_mix[gene]))
-              for gene in concat_order]
+    # now we remake the sequence again with the updated calls
+    mixseq = []
+    for gene in concat_order:
+        # need to sort the positions, NOTE keys are strings!!!!
+        ordered_pos = [int(p)
+                       for p in mix_seq_calls[mix_sample_name][gene].keys()]
+        ordered_pos.sort()
+        for pos in ordered_pos:
+            for nt in mix_seq_calls[mix_sample_name][gene][str(pos)]:
+                mixseq.append(nt)
+
+
+       # mixseq = [remove_primers(gene, "".join(seq_dict_mix[gene]))
+    #           for gene in concat_order]
     mixseq = write_seq(mix_sample_id, "".join(mixseq), desc=fil)
     seq_outfile.write(mixseq)
 seq_outfile.close()
